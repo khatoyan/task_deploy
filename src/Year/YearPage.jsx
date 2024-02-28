e4f832f39c849dd92867fe61de2a4ed04224bf42
Arman Khatoyan <94473802+khatoyan@users.noreply.github.com>
2024-02-28 23:18:31 +0700

    feat: add year update button

--- src/Year/YearPage.jsx
+++ src/Year/YearPage.jsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
+import { Button } from '../Button/Button';
+
 import styles from './Year.module.css';
 
 export const Year = () => {
     const [date, setDate] = useState('');
-    
+    const [isClicked, setClicked] = useState(false);
+
     useEffect(() => {
         const currDate = new Date();
-        const date = currDate.getFullYear() + '-' + (currDate.getMonth() + 1) + '-' + currDate.getDate();
+        const time = currDate.getHours() + ":"  
+                    + currDate.getMinutes() + ":" 
+                    + currDate.getSeconds();
+        const date = currDate.getFullYear() + '-' 
+                    + (currDate.getMonth() + 1) + '-' 
+                    + currDate.getDate() + ", " + time;
+        
         setDate(date);
-    }, []);
+    }, [isClicked]);
 
-
+    const handleClick = () => {
+        setClicked(!isClicked);
+    }
+    
     return (
         <main className={styles.main}>
             <p className={styles.data}>
-                Текущая дата: {date}
+                Текущая дата: <span className={styles.date}>{date}</span>
             </p>
+            <Button onClick={handleClick}>Обновить</Button>
         </main>
     );
 };
